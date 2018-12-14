425ead27d549555c36fdb657404addd25b2bf37a
Ciro <ciromoraismedeiros@gmail.com>
2018-12-14 15:46:44 -0300

    Zero-ed memory allocation

--- src/util.c
+++ src/util.c
@@ -7,7 +7,7 @@
 #include <string.h>
 #include "util.h"
 void *checked_malloc(int len)
-{void *p = malloc(len);
+{void *p = calloc(1, len);
  if (!p) {
     fprintf(stderr,"\nRan out of memory!\n");
     exit(1);
